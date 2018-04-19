0dd5c7e15a2311e7db87da63022a6ec98a1eccf2
Adam Recvlohe <adam.recvlohe@gmail.com>
2018-04-19 15:57:47 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import Calendar from "react-big-calendar";
+import moment from "moment";
+
+import "./App.css";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import logo from "./logo.svg";
+
+Calendar.setLocalizer(Calendar.momentLocalizer(moment));
+
+class App extends Component {
+  state = {
+    events: [
+      {
+        start: new Date(),
+        end: new Date(moment().add(1, "days")),
+        title: "Some title"
+      }
+    ]
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Calendar
+          defaultDate={new Date()}
+          defaultView="month"
+          events={this.state.events}
+          style={{ height: "100vh" }}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;

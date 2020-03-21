72aea358fb5c66a5756909eb696bb69b053a4fb1
Adam Recvlohe <adam.recvlohe@gmail.com>
2020-03-21 00:20:25 -0500

    Update deps

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React, { Component } from "react";
-import {
-    Calendar,
-    momentLocalizer,
-  } from 'react-big-calendar';
+import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 
 import "./App.css";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
-const localizer = momentLocalizer(moment)
-  
+const localizer = momentLocalizer(moment);
+
 class App extends Component {
   state = {
     events: [
       {
-        start: new Date(),
-        end: new Date(moment().add(1, "days")),
+        start: moment().toDate(),
+        end: moment()
+          .add(1, "days")
+          .toDate(),
         title: "Some title"
       }
     ]
